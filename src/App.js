71349d86a346336e7b617c1f2b71c332bb8a1ad8
Seojisoo20191941 <amy0480@naver.com>
2022-05-29 01:29:00 +0900

    studying state : what is state?

--- src/App.js
+++ src/App.js
@@ -13,16 +13,23 @@
 
 // export default App;
 // import MyComponent from './MyComponent';
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Box from './box.jsx';
 
 function App() {
+  let count = 0;
+  const [count2, setCount2] = useState();
+  const increase = () => {
+    count = count + 1;
+  };
   return (
     <main>
-      <Box name="지수" num="1" />
+      {/* <Box name="지수" num="1" />
       <Box name="지원" num="2" />
-      <Box name="선영" num="3" />
+      <Box name="선영" num="3" /> */}
+      <div>{count}</div>
+      <button onClick={increase}>증가</button>
     </main>
   );
 }
