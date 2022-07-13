4517716ab0cb5be215c849ce5ff737c6a66c6d73
Seojisoo20191941 <amy0480@naver.com>
2022-07-13 13:27:08 +0900

    studied useState

--- src/App.js
+++ src/App.js
@@ -1,19 +1,7 @@
-import React, { useState } from 'react';
-
+import React from 'react';
+import Say from './Say';
 const App = () => {
-  let count = 0;
-  const [count2, setCount2] = useState(0);
-  const increase = () => {
-    count = count + 1;
-    setCount2(count2 + 1);
-  };
-  return (
-    <div>
-      <div>{count}</div>
-      <div>state:{count2}</div>
-      <button onClick={increase}>증가</button>
-    </div>
-  );
+  return <Say />;
 };
 
 export default App;
