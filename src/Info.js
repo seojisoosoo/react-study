b32d7387f0a0f40f0d21e49ce9dde567abd22113
Seojisoo20191941 <amy0480@naver.com>
2022-07-17 03:33:12 +0900

    useEffect

--- src/Info.js
+++ src/Info.js
@@ -1,8 +1,13 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Info = () => {
   const [name, setName] = useState('');
   const [nickname, setNickname] = useState('');
+
+  useEffect(() => {
+    console.log('마운트될 때만 실행됩니다');
+  }, []);
+
   const onChangeName = (e) => {
     setName(e.target.value);
   };
