ee9cb7c5788e3c98ff6932f3b33cf466b1cb8d10
Seojisoo20191941 <amy0480@naver.com>
2022-07-17 04:45:29 +0900

    ~329 routing

---
+++ router-tutorial/src/pages/Home.js
@@ -0,0 +1,19 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
+
+const Button = styled.button`
+  background-color: blue;
+`;
+const Home = () => {
+  return (
+    <>
+      <h1>홈</h1>
+      <Link to="/about">
+        <Button>소개</Button>
+      </Link>
+    </>
+  );
+};
+
+export default Home;
